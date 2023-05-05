63777978ad5f600f0c4d47881079173f835ccf12
Leevi Koskinen <leevi.koskinen2@metropolia.fi>
2023-05-05 11:39:02 +0300

    readying for Azure

--- src/app.ts
+++ src/app.ts
@@ -90,6 +90,9 @@ app.use(express.json());
         },
       })
     );
+    app.get("/*", (req, res) => {
+      res.sendFile(path.join(__dirname, "public", "index.html"));
+    });
     app.get("/", (req, res) => {
       res.sendFile(path.join(__dirname, "public", "index.html"));
     });
